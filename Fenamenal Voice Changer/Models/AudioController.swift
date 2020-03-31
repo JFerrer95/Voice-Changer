d801d86c262cdac7ffca1ecd356e74ee4b1b8d7f
JFerrer95 <jonathanferrer95@gmail.com>
2020-03-30 21:37:49 -0400

    Figured out recording

---
+++ Fenamenal Voice Changer/Models/AudioController.swift
@@ -0,0 +1,11 @@
+//
+//  AudioController.swift
+//  Fenamenal Voice Changer
+//
+//  Created by jonathan ferrer on 3/30/20.
+//  Copyright © 2020 Jonathan Ferrer. All rights reserved.
+//
+
+import Foundation
+
+import AudioKit
